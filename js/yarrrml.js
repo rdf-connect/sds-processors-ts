4f1951a332460ac98f6031543f369e0717b10357
ajuvercr <arthur.vercruysse@ugent.be>
2023-05-08 12:14:16 +0200

    update bucketizers

--- js/yarrrml.js
+++ js/yarrrml.js
@@ -2,19 +2,20 @@ const Y2R = require('@rmlio/yarrrml-parser/lib/rml-generator.js');
 const N3 = require('n3');
 
 function yarrml(reader, writer) {
-  console.log(reader, writer);
+
   const handle = (x) => {
     const y2r = new Y2R();
     const triples = y2r.convert(x);
 
     const str = new N3.Writer().quadsToString(triples);
-    console.log(str);
     return writer.push(str);
   }
+
   reader.data(handle);
   if(reader.lastElement) {
     handle(reader.lastElement);
   }
+
 }
 
 module.exports.yarrml = yarrml;
--- src/generator.ts
+++ src/generator.ts
@@ -12,6 +12,9 @@ const types = [
   { x: 3, y: 4 },
   { x: 2, y: 2 },
   { x: 3, y: 3 },
+  { x: 4, y: 4 },
+  { x: 5, y: 5 },
+  { x: 6, y: 6 },
 ];
 
 function generateMember(i: number, includeT: boolean) {
